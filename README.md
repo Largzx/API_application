627b796f45210ac10c1975d0231ef5af854fd5ab
Largzx <2817407674@qq.com>
2025-03-08 17:55:34 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# API＋streamlit应用项目
+
+使用deepseekAPI，构建后端方法，通过streamlit构建前端操作界面
+
+- 输入自己deepseek的API密钥
+- 可以选择deepseek的不同模型
+- 输入稿子的话题
+- 选择演讲时长以及模型温度参数（即文本是否更加多样性）
+- 后期可以添加参数presence_penalty来使模型生成更多新词
+
+![界面展示](/img.png)
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with st.sidebar:
 
     st.markdown("[获取Deepseek API密钥](https://platform.deepseek.com/)")
 
-subject = st.text_input("请输入脱口秀的主题")
+subject = st.text_input("请输入脱口秀的话题")
 talk_length = st.number_input("请输入脱口秀演讲的大致时长（单位：分钟）", min_value=1.0, step=1.0)
 creativity = st.slider("输入脱口秀文本的创造力（数字小更严谨，数字大更加具有多样性）", min_value=0.0,
                        max_value=1.5, value=0.2, step=0.1)
